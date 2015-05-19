d89bba7e1bb41588a052ca4520fc337200e0f6d5
kenning <hwerzog@gmail.com>
2015-05-19 09:52:32 -0700

    Moved express to root folder and deleted unimportant files

--- server.js
+++ server.js
@@ -7,31 +7,20 @@ var bodyParser = require('body-parser');
 
 var mongo = require('mongodb');
 var monk = require('monk');
-var dbConfig = require('../db/mongolab.config'); 
-var db = monk(connectionString);
-
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var dbConfig = require('./db/mongolab.config'); 
+var db = monk(dbConfig);
 
 var app = express();
 
 var questionList = db.get('TestQuestions');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
